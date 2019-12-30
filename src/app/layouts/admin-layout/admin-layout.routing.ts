7614113f07869236bf3547020ad5e8c850455ef6
Cihan Ağırbaş <uagirbas@netas.com.tr>
2019-12-30 11:51:45 +0300

    first commit

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { XoxMainComponent } from 'app/pages/xox-main/xox-main.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'xox-main',       component: XoxMainComponent}
+];
---
+++ src/app/pages/xox-main/xox-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xox-main',
+  moduleId: module.id,
+  templateUrl: './xox-main.component.html',
+  styleUrls: ['./xox-main.component.css']
+})
+export class XoxMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
