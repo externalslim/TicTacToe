978d69a3f3fef41ced317e2027ab0a85a014b3a6
Cihan Ağırbaş <uagirbas@netas.com.tr>
2019-12-31 15:07:34 +0300

    folder system.

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { XoxMainComponent } from 'app/pages/xox-main/xox-main.component';
-
-export const AdminLayoutRoutes: Routes = [
-    { path: 'xox-main',       component: XoxMainComponent}
-];
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'produrl',
-  services:{
-      login_service: ''
-  }
-};
